83b8ec9402c4d4229ce26847abdf86f10a94f23d
Youssof-lab <youssof@dal.ca>
2022-01-11 20:22:02 -0400

    Added entry point files

---
+++ src/index.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const Discord = require("discord.js");
+const config = require("./config.json");
+const logger = require("./util/Logger");
+// const eventHandler = require("./events");
+// const commandHandler = require("./commands");
+
+class LeagueBot extends Discord.Client {
+  constructor(clientOptions = {}) {
+    super(clientOptions);
+
+    this.discord = Discord;
+    // this.commands = new this.discord.Collection();
+    this.slashCommands = new this.discord.Collection();
+
+    this.config = config;
+  }
+  async start() {
+    logger.info("Starting...");
+    // eventHandler.setup(this);
+    // commandHandler.registerSlashCommands(this.slashCommands);
+    this.login(process.env.TOKEN).catch((err) => logger.error(err))
+  }
+}
+
+const bot = new LeagueBot({
+  allowedMentions: {
+    parse: ["roles", "users"],
+    repliedUser: false,
+  },
+  presence: {
+    activities: [
+      {
+        name: `v${config.version}`,
+        type: "PLAYING"
+      }
+    ],
+  },
+  intents: [
+    "GUILDS",
+    "GUILD_MEMBERS",
+    "GUILD_PRESENCES",
+    "GUILD_MESSAGES",
+    "GUILD_MESSAGE_REACTIONS",
+  ],
+  partials: ["MESSAGE", "CHANNEL"],
+});
+
+bot.start();
---
+++ src/util/Logger.js
@@ -0,0 +1,43 @@
+const { createLogger, format, transports } = require("winston");
+const { printf, combine, errors, timestamp, colorize } = format;
+require("winston-daily-rotate-file");
+
+const customFormat = printf((info) => {
+  let { timestamp, level, stack, message } = info;
+  message = stack || message;
+  return `[${timestamp}] ${level} | ${message}`;
+});
+
+const logger = createLogger({
+  level: "info",
+  format: combine(
+    format((info) => {
+      info.level = info.level.toUpperCase();
+      return info;
+    })(),
+    errors({ stack: true }),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" })
+  ),
+  transports: [
+    new transports.Console({
+      format: combine(colorize(), customFormat),
+    }),
+    new transports.DailyRotateFile({
+      filename: "out-%DATE%.log",
+      dirname: "./logs/",
+      datePattern: "YYYY-MM-DD",
+      json: false,
+      format: customFormat,
+    }),
+    new transports.DailyRotateFile({
+      level: "warn",
+      filename: "err-%DATE%.log",
+      dirname: "./logs/",
+      datePattern: "YYYY-MM-DD",
+      json: false,
+      format: customFormat,
+    })
+  ]
+});
+
+module.exports = logger;
